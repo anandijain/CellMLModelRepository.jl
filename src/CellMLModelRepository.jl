bd80e7ee95df6498912a853aa721e0e1e255e6f0
Anand <anandj@uchicago.edu>
2021-04-07 22:07:12 -0700

    mkpath

--- src/CellMLModelRepository.jl
+++ src/CellMLModelRepository.jl
@@ -5,6 +5,7 @@ using JSON3, JSONTables, CSV, DataFrames
 using Base.Threads, Downloads
 
 const datadir = joinpath(@__DIR__, "../data/")
+mkpath(datadir)
 
 function cellml_metadata(;json_path = "$(datadir)cellml_exposures.json", csv_path="$(datadir)exposures_metadata.csv")
     run(`curl -sL -H 'Accept: application/vnd.physiome.pmr2.json.1' https://models.physiomeproject.org/search -d '{
@@ -24,7 +25,6 @@ end
 
 "anands preferred method of downloading. no boo boo html parsing"
 function cellml_models(;dir = "$(datadir)cellml_models/", csv_path="$(datadir)exposures_metadata.csv")
-    mkpath(dir)
     if isfile(csv_path) 
         df = CSV.read(csv_path, DataFrame)
     else
