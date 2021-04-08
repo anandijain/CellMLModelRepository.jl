c28fb19d6abda5e7c44289023b204168b3be0b06
Anand <anandj@uchicago.edu>
2021-04-07 21:21:21 -0700

    limit to testing 10 fns

--- src/CellMLModelRepository.jl
+++ src/CellMLModelRepository.jl
@@ -2,9 +2,9 @@
 module CellMLModelRepository
 
 using JSON3, JSONTables, CSV, DataFrames
-using Base.Threads
+using Base.Threads, Downloads
 
-function curl_metadata(;json_path = "data/cellml_exposures.json", csv_path="data/exposures_metadata.csv")
+function cellml_metadata(;json_path = "data/cellml_exposures.json", csv_path="data/exposures_metadata.csv")
     run(`curl -sL -H 'Accept: application/vnd.physiome.pmr2.json.1' https://models.physiomeproject.org/search -d '{
            "template": {"data": [
                {"name": "Subject", "value": "CellML Model"},
@@ -21,19 +21,19 @@ function curl_metadata(;json_path = "data/cellml_exposures.json", csv_path="data
 end
 
 "anands preferred method of downloading. no boo boo html parsing"
-function curl_cellml_models(;dir = "data/cellml_models/", csv_path="data/exposures_metadata.csv")
+function cellml_models(;dir = "data/cellml_models/", csv_path="data/exposures_metadata.csv")
     mkpath(dir)
     if isfile(csv_path) 
         df = CSV.read(csv_path, DataFrame)
     else
-        df = curl_metadata(;csv_path=csv_path)
+        df = cellml_metadata(;csv_path=csv_path)
     end
     urls = map(x->x[1:end-5], df.href)
     @sync Threads.@threads for url in urls
-        download(url, "$(dir)$(splitdir(url)[end])")
+        Downloads.download(url, "$(dir)$(splitdir(url)[end])")
     end
 end
 
-export curl_cellml_models
+export cellml_models, cellml_metadata
 
 end
--- test/data.jl
+++ test/data.jl
@@ -2,9 +2,12 @@ using CellMLModelRepository
 using CSV, DataFrames
 using Test
 
+df = cellml_metadata()
+@test df isa DataFrame
+
 # test suite stuff
 curl_cellml_models()
-fns = readdir(joinpath(@__DIR__, "../data/cellml_models/"); join=true)
+fns = readdir(joinpath(@__DIR__, "../data/cellml_models/"); join=true)[1:10] # only do 10
 @test isfile(fns[1])
 
 using Pkg
